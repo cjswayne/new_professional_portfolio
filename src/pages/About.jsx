81084e775ab61da5972ad6b469f38d2b484d5db2
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-23 17:25:46 -0800

    finished about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,20 @@
+import aboutPic from '../assets/myself-square.jpg'
+
+
 function About() {
     return (
-        <>
-            <h1 className="w-10 mA">About</h1>
-        </>
+        <div className="w-50 mA tc">
+            <h1>About Me</h1>
+            <div className="flex flex-row items-center" >
+            <div>
+            <img src={aboutPic} alt="image of charles swayne" className="w-100"></img>
+            </div>
+            <p className="ma4">
+                            I've always found web design fascinating, which inspired me to pursue a career in frontend development. My approach to problem-solving is straightforward yet effective: start, experiment, and iterate. This method has served me well, particularly in my recent project, 'Animal Blog Navigation' on reptichip.com. This project was a culmination of all the web development skills I've acquired so far, and I'm really proud of the outcome. Looking ahead, my goal is to expand my expertise and become a full-stack developer, blending front-end creativity with back-end functionality.
+                        </p>
+            </div>
+            
+        </div>
     )
 }
 
